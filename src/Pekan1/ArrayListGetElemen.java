b8adc04e1357c10cba84a4f7975614a1db0b07ae
hp <hp@FTI-LAB03-CAD1>
2025-04-29 12:19:26 +0700

    Pratikum pekan 2

---
+++ src/Pekan1/ArrayListGetElemen.java
@@ -0,0 +1,17 @@
+package Pekan1;
+
+import java.util.*;
+
+public class ArrayListGetElemen {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		list.add(9);
+		list.add(5);
+		list.add(6);
+		System.out.println(list);
+		Integer n = list.get(1);
+		System.out.println("Pada indeks ke 1 angkanya adalah:" + n);
+	}
+
+}
---
+++ src/Pekan1/MahasiswaMain.java
@@ -0,0 +1,68 @@
+package Pekan1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.*;
+
+public class MahasiswaMain {
+	private Object nim;
+
+	public static void main (String[] args) {
+		ArrayList<MahasiswaMain> mahasiswaList = new ArrayList<>();
+		Scanner scanner = new Scanner (System.in);
+		int choice;
+		do {
+			System.out.println("Menu: ");
+			System.out.println("1. Tambah Mahasiswa");
+			System.out.println("2. Tampilkan Semua Mahasiswa");
+			System.out.println("3. Hapus Mahasiswa Berdasarkan NIM");
+			System.out.println("4. Cari Mahasiswa Berdasarkan NIM");
+			System.out.println("5. Keluar");
+			System.out.println("Pelih Menu: ");
+			choice = scanner.nextInt();
+			scanner.nextLine(); //Consume newLine
+			switch (choice) {
+				case 1:
+					System.out.println("Masukan NIM: ");
+					String nim = scanner.nextLine();
+					System.out.println("Masukan Nama: ");
+					String nama = scanner.nextLine();
+					System.out.println("Masukan Prodi: ");
+					String prodi = scanner.nextLine();
+					mahasiswaList.add(new MahasiswaMain());
+					break;
+				case 2:
+					System.out.println("Data Mahasiswa: ");
+					for (MahasiswaMain mhs : mahasiswaList)	{
+						System.out.println(mhs);
+					}
+					break;
+				case 3:
+					System.out.println("Masukan NIM yang akan di hapus: ");
+					String nimHapus = scanner.nextLine();
+					mahasiswaList.removeIf(mhs ->mhs.nim.equals(nimHapus));
+					break;
+				case 4:
+					System.out.println("Masukan NIM yang di cari: ");
+					String nimCari = scanner.nextLine();
+					for (MahasiswaMain mhs : mahasiswaList) {
+						if (mhs.nim.equals(nimCari)) {
+							System.out.println(mhs);
+							break;
+						} else {
+							System.out.println("nim tidak ada");
+						}
+					}
+					break;
+				case 5:
+					System.out.println("Keluar dari program.");
+					break;
+					default:
+						System.out.println("Pilihan tidak valid.");
+			}
+		} while (choice != 5);
+		
+		scanner.close();
+	}
+
+}
