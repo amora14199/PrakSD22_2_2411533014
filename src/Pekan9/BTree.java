fe5ea94f0d68bae14a1e302c934662fce9b53e60
ASUS <ASUS@LAPTOP-SUB936O7>
2025-07-02 18:28:23 +0700

    Pekan9

---
+++ src/Pekan9/BTree.java
@@ -0,0 +1,10 @@
+package Pekan9;
+
+public class BTree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Pekan9/GraphTraversal.java
@@ -0,0 +1,10 @@
+package Pekan9;
+
+public class GraphTraversal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
